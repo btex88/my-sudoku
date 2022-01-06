013df1151689c38319a9b61c24eb0d3fb67441c1
bruno teixeira <btex88@gmail.com>
2022-01-06 19:34:40 -0300

    feat(PAGE): Not found page implemented

--- src-completed-files/pages/not-found.jsx
+++ src-completed-files/pages/not-found.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import notFound from '../images/not-found.png';
 
 class NotFound extends React.Component {
   render() {
     return (
-      <div>
-        
+      <div className="w-full h-full flex items-center justify-center bg-pink-50">
+        <img className="object-contain" src={notFound} alt="not found" />
       </div>
     );
   }
